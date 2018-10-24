21a34ab9ac96b61405308f3c3de2777e3dd72d94
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-10-24 19:27:08 +0100

    add material design

--- JustRipe/JustRipe/Pages/Dashboard.xaml.cs
+++ JustRipe/JustRipe/Pages/Dashboard.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace JustRipe
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Dashboard.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Dashboard : Window
     {
-        public MainWindow()
+        public Dashboard()
         {
             InitializeComponent();
         }
