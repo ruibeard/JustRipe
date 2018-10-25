abe850d48c1970525edfd45ab722f213b6553d9e
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-10-25 02:36:09 +0100

    add first darft Login page

--- JustRipe/JustRipe/Pages/Dashboard.xaml.cs
+++ JustRipe/JustRipe/Pages/Dashboard.xaml.cs
@@ -23,5 +23,34 @@ namespace JustRipe
         {
             InitializeComponent();
         }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserControl usc = null;
+            GridMain.Children.Clear();
+
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                case "ItemHome":
+                    break;
+                case "ItemCreate":
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
