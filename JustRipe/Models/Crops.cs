648d96747c73ed3f7f8be6572c699279914612ab
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-11-01 10:30:01 +0000

    Added new field to crop model

---
+++ JustRipe/Models/Crops.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustRipe.Models
+{
+    class Crops : ObservableObject
+    {
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+                    RaisePropertyChanged("Name");
+                }
+            }
+
+        }
+    }
+}
