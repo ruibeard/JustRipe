3f3470fc9f3d04f21020f27b5f223798b6f6dcdb
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-11-01 10:49:26 +0000

    Add creation date and updated date to sql tables, every table should have this

--- JustRipe/_Schema/dbo.Crop.sql
+++ JustRipe/_Schema/dbo.Crop.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[Crop] (
-    [Id]   INT           NOT NULL,
-    [Name] VARCHAR (255) NOT NULL,
-    [Created_At] DATETIME2 NULL DEFAULT CURRENT_TIMESTAMP, 
-    [Updated_At] DATETIME2 NULL DEFAULT CURRENT_TIMESTAMP, 
+    [Id]         INT           NOT NULL,
+    [Name]       VARCHAR (255) NOT NULL,
+    [Created_At] DATETIME2 (7) DEFAULT (getdate()) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
