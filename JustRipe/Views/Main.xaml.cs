1118626af085e4843800b1ec0e0fc044bd6e18ec
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-11-04 14:59:53 +0000

    deletes pages folder

--- JustRipe/Views/Main.xaml.cs
+++ JustRipe/Views/Main.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace JustRipe
+namespace JustRipe.Views
 {
     /// <summary>
     /// Interaction logic for Dashboard.xaml
