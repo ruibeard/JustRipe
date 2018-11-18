dee21d6345cfc399d8a3e470d2f51c4a7ff0e3fe
Rui Almeida <ruitiagoribeiroalmeida@gmail.com>
2018-11-18 17:21:27 +0000

    Using Dapper SQLITE

--- JustRipe/Data/Repositories/CropRepository.cs
+++ JustRipe/Data/Repositories/CropRepository.cs
@@ -23,38 +23,40 @@ namespace JustRipe.Data.Repositories
         {
             return from crop in repository.GetAll()
                    select new Crop()
-            {
-                Name = crop.Name,
-                Stage = crop.Stage,
-                Area = crop.Area,
-                Type = crop.Type,
-            };
+                   {
+                       Name = crop.Name,
+                       Stage = crop.Stage,
+                       Area = crop.Area,
+                       Type = crop.Type,
+                   };
 
-            return repository.GetAll().Select(crop => new Crop()
-            {
-                Name = crop.Name,
-                Stage = crop.Stage,
-                Area = crop.Area,
-                Type = crop.Type,
-            });
+            //return repository.GetAll().Select(crop => new Crop()
+            //{
+            //    Name = crop.Name,
+            //    Stage = crop.Stage,
+            //    Area = crop.Area,
+            //    Type = crop.Type,
+            //});
         }
 
         //public IEnumerable<Crop> GetCropsByUser(string username)
         //{
-        //    var crops = 
+        //    var crops =
         //        from crop in repository.GetAll()
         //        join user in repositoryUser.SearchFor(x => x.UserName == username) on crop.Name equals user.UserName
         //        where user.UserName == username
         //        select crop;
 
-        var crops =
-            from crop in repository.GetAll()
-            join user in repositoryUser.GetAll() on crop.Name equals user.UserName
-            where user.UserName == username
-            select crop;
-    }
+        //    return
+        //    from crop in repository.GetAll()
+        //    join user in repositoryUser.GetAll() on crop.Name equals user.UserName
+        //    where user.UserName == username
+        //    select crop;
 
-    public void Dispose()
+
+        //}
+
+        public void Dispose()
         {
             repository.Dispose();
         }
